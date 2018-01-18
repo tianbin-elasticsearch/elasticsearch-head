5ecd84f6f364f82fe1cca6ea963b92657b29e8d0
tianbin <Tj!634346496#qq>
2018-01-18 15:13:33 +0800

    服务器地址

--- _site/app.js
+++ _site/app.js
@@ -4351,7 +4351,7 @@
 		init: function(parent) {
 			this._super();
 			this.prefs = services.Preferences.instance();
-			//server url
+			//server  url
 			this.base_uri = this.config.base_uri || this.prefs.get("app-base_uri") || "http://127.0.0.1:9200";
 			if( this.base_uri.charAt( this.base_uri.length - 1 ) !== "/" ) {
 				// XHR request fails if the URL is not ending with a "/"
